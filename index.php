5f27cfb38f224c6f29da86ad8c629a8afc2d40f9
Andres Ivanov <narva-lf@ya.ru>
2016-08-18 09:08:59 +0300

    Start

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ if ($_SERVER['REQUEST_URI'] == '/') {
 }
 else {
     $URL_Path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-    $URL_Parts = explode('/', trim($URL_Path, '/'));
+    $URL_Parts = explode('/', trim($URL_Path,   ' /'));
     $Page = array_shift($URL_Parts);
     $Module = array_shift($URL_Parts);
 
