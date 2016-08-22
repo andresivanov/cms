8a5a3f419e0e1bd524d12ea28a8b70ebca2acc39
Andres Ivanov <narva-lf@ya.ru>
2016-08-22 03:23:08 +0300

    Ошибка в account.php найдена, всё ОК.

--- form/account.php
+++ form/account.php
@@ -9,13 +9,17 @@ if ($Module == 'register' and $_POST['enter']) {
 
     if (!$_POST['login'] or !$_POST['email'] or !$_POST['password'] or !$_POST['name'] or $_POST['country'] > 4)
         exit ('Ошибка валидации формы');
-    $Row = mysqli_fetch_assoc(mysqli_query($CONNECT, "SELECT login FROM users WHERE login = $_POST['login']"))
+
+
+
+
+    $Row = mysqli_fetch_assoc(mysqli_query($CONNECT, "SELECT `login` FROM `users` WHERE `login` = '$_POST[login]'"));
     if ($Row['login'])
         exit ('Логин <b>'.$_POST['login'].'</b> уже используется');
-
+    $Row = mysqli_fetch_assoc(mysqli_query($CONNECT, "SELECT `email` FROM `users` WHERE `email` = '$_POST[email]'"));
     if ($Row['login'])
         exit ('Е-Майл <b>'.$_POST['email'].'</b> уже используется');
-    mysqli_query($CONNECT, "INSERT INTO users VALUE ('', '$_POST[login]', $_POST[password], $_POST[name], NOW(), $_POST[email], $_POST[country], $_POST[avatar])")
+    mysqli_query($CONNECT, "INSERT INTO `users` VALUES ('', '$_POST[login]', '$_POST[password]', '$_POST[name]', NOW(), '$_POST[email]', $_POST[country], $_POST[avatar])");
     echo 'OK';
 }
 ?>
